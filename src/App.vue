93125d882317a5637e15b9bca94a8685e57d98a1
Eshatskov <shatskov@keeneye.pro>
2022-09-15 12:25:34 +0300

    create CardItem, add styles to cards, get posts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <CardItem v-for="i of this.posts" :key="i.id" :post="i" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardItem from "./components/CardItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    async getPosts() {
+      const postsFetched = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      // postsFetched.then((response) => (this.posts = response));
+      postsFetched.json().then((response) => (this.posts = response));
+    },
+  },
+  mounted() {
+    this.getPosts();
+  },
+
   components: {
-    HelloWorld
-  }
-}
+    CardItem,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  gap: 15px;
+  flex-wrap: wrap;
 }
 </style>
