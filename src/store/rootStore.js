a71c97d0146cc56055bfd067f588a23be6436b3c
Eshatskov <shatskov@keeneye.pro>
2022-09-29 17:30:39 +0300

    bind store with NewPost - add state isAddNewPost and mutations to store - either visible form if isAddNewPost or placeholderNewItem - add styles

--- src/store/rootStore.js
+++ src/store/rootStore.js
@@ -4,6 +4,7 @@ export const store = createStore({
   state: {
     favoritePosts: {},
     posts: {},
+    isAddNewPost: false
   },
   mutations: {
     setIsLikeOfPost({ posts }, postId) {
@@ -17,13 +18,20 @@ export const store = createStore({
       delete favoritePosts[postId]
       posts[postId].isLike = false
     },
-    setPosts(state, posts) {
-      if (posts)
-        state.posts = posts.reduce((obj, post) => {
+    setPosts(state, fetchPosts) {
+      if (fetchPosts)
+       state.posts=fetchPosts.reduce((obj, post) => {
           obj[post?.id] = { ...post, isLike: false }
           return obj
         }, {})
     },
+    setNewPost({posts},post){
+      const id=Object.keys(posts).length + 1
+      posts[id]={...post, id}
+    },
+    setIsAddNewPost(state){
+      state.isAddNewPost=!state.isAddNewPost
+    }
   },
   actions: {
     async getPosts({ commit }) {
@@ -38,5 +46,16 @@ export const store = createStore({
       commit("setIsLikeOfPost", post.id)
       commit("setFavoritePosts", post)
     },
+   async addNewPost({commit}, newPost){
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(newPost),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => { commit('setNewPost', json) });
+    }
   },
 })
